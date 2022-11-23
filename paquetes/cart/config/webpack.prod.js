539b3b0b3e9922944a18bd894c17ca1076e765aa
danilousach <danilo.verdugo@usach.cl>
2022-11-23 02:29:14 -0300

    modified:   .github/workflows/container.yml

--- paquetes/cart/config/webpack.prod.js
+++ paquetes/cart/config/webpack.prod.js
@@ -7,6 +7,7 @@ const prodConfig = {
   mode: 'production',
   output: {
     filename: '[name].[contenthash].js',
+    publicPath: '/cart/latest/',
   },
   plugins: [
     new ModuleFederationPlugin({
--- paquetes/products/config/webpack.prod.js
+++ paquetes/products/config/webpack.prod.js
@@ -9,6 +9,7 @@ const prodConfig = {
   mode: 'production',
   output: {
     filename: '[name].[contenthash].js',
+    publicPath: '/products/latest/',
   },
   plugins: [
     new ModuleFederationPlugin({
