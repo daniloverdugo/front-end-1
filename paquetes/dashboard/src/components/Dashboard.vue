9dcf951570a7b6fdd79544a84ce601f75b42d014
danilousach <danilo.verdugo@usach.cl>
2022-11-28 18:09:29 -0300

    modified:   paquetes/dashboard/src/components/Dashboard.vue

--- paquetes/dashboard/src/components/Dashboard.vue
+++ paquetes/dashboard/src/components/Dashboard.vue
@@ -10,8 +10,12 @@
     <p>nombre formato: {{nombreFormateado}}</p>
     <p>nombre formato método: {{nombreFormateadoMetodo()}}</p>
 
-    <button v-on:click="contador += 1">clic</button>
+    <button v-on:click.once="contador += 1">clic</button>
     <p>has hecho {{contador}} clics</p>
+    <button v-on:click="saludar('Pedro',$event)">hi</button>
+    <div class="myDiv" v-bind:class=claseDinamica @click="this.active = !this.active">
+      activo?:{{active}}
+    </div>
   </div>
 </template>
 
@@ -28,11 +32,17 @@
         precio:1500,
         apellido:"luLIn",
         contador:0,
+        active:false,
     }
     },
     methods: {
         sumar(){
           return this.precio + 1000
+        },
+        saludar(nombre, e){
+          alert("hi " + nombre)
+          console.log(e);
+          this.contador= 0
         },
       nombreFormateadoMetodo(){
         return this.nombre.charAt(0).toUpperCase() + this.nombre.slice(1).toLocaleLowerCase() + " " + Date.now()
@@ -45,10 +55,26 @@
       nombreFormateado(){
         return this.nombre.charAt(0).toUpperCase() + this.nombre.slice(1).toLocaleLowerCase() + " " + Date.now()
       },
+      claseDinamica(){
+        return this.active?"active":"noactive";
+      }
     },
   }
 </script>
 
 <style>
-
+  .myDiv{
+    width: 180px;
+    height: 60px;
+    padding: 10px;
+    text-align: center;
+    line-height: 30px;
+    margin: 10px;
+  }
+  .active{
+    border: solid 3px #a01aa2
+  }
+  .noactive{
+    border: solid 3px #555555
+  }
 </style>
