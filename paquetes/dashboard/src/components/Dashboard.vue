10ed6fa9e4995469f88c815980ca372fa5c1380b
danilousach <danilo.verdugo@usach.cl>
2022-11-28 22:45:41 -0300

    modified:   paquetes/dashboard/package-lock.json

--- paquetes/dashboard/src/components/Dashboard.vue
+++ paquetes/dashboard/src/components/Dashboard.vue
@@ -21,16 +21,18 @@
       <p>{{contador}}</p>
     </template>
     <p v-else>valor cero</p>
+
+    <button v-on:click="addPersona()">persona</button>
+    <button v-on:click="getPersonas()">personas</button>
   </div>
 </template>
 
 <script>
-  import 'primevue/resources/themes/saga-blue/theme.css';
-  import 'primevue/resources/primevue.css';
-  import 'primeflex/primeflex.css';
-  import 'primeicons/primeicons.css';
-
+import axios from 'axios';
+import { ref } from 'vue';
   export default {
+    setup(){ 
+        },
     data() {
       return{
         nombre: "holanda q talca",
@@ -49,9 +51,33 @@
           console.log(e);
           this.contador= 0
         },
-      nombreFormateadoMetodo(){
-        return this.nombre.charAt(0).toUpperCase() + this.nombre.slice(1).toLocaleLowerCase() + " " + Date.now()
-      },
+        nombreFormateadoMetodo(){
+          return this.nombre.charAt(0).toUpperCase() + this.nombre.slice(1).toLocaleLowerCase() + " " + Date.now()
+        },
+        addPersona(){
+          const persona= {
+            name:"julin",
+            job:"jobin"
+          }
+          axios.post('https://reqres.in/api/users', persona)
+          .then(res => console.log(res))
+          .catch(error => console.log(error))
+        },
+        getPersonas(){
+        const personas = ref([]);         
+        axios.get('https://reqres.in/api/users')
+          .then(res => {
+           for (const id in res.data.data){
+              personas.value.push({
+                id:  res.data.data[id].id,
+                email: res.data.data[id].email,
+                avatar:res.data.data[id].avatar
+              })
+            }
+            console.log(personas.value);
+          })
+          .catch(error => console.log(error))
+        },
     },
     computed: {
       nombreCompleto:function(){
