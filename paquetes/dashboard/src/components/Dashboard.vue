7ab3a87dc99a766e71b04633555a35ce0cba8b4a
danilousach <danilo.verdugo@usach.cl>
2022-11-28 23:44:16 -0300

    modified:   paquetes/container/public/index.html modified:   paquetes/dashboard/src/components/Dashboard.vue

--- paquetes/dashboard/src/components/Dashboard.vue
+++ paquetes/dashboard/src/components/Dashboard.vue
@@ -1,14 +1,78 @@
 <template>
-  <div>
-    <h1>{{nombre}}  {{sumar()}}</h1>    
-    <p>nombre <input type="text" v-model="nombre"></p>
-    <p>apellido <input type="text" v-model="apellido"></p>
+  <div class="p-grid p-fluid dashboard">
+    <div class="p-col-12 p-lg-4">
+      <div class="card summary">
+        <h1>{{nombre}}  {{sumar()}}</h1>    
+        <p>nombre <input type="text" v-model="nombre"></p>
+        <p>apellido <input type="text" v-model="apellido"></p>
 
-    <p>nombre: {{nombre}}</p>
-    <p>apellido: {{apellido}}</p>
-    <p>nombre completo: {{nombreCompleto}}</p>
-    <p>nombre formato: {{nombreFormateado}}</p>
-    <p>nombre formato método: {{nombreFormateadoMetodo()}}</p>
+        <p>nombre: {{nombre}}</p>
+        <p>apellido: {{apellido}}</p>
+        <p>nombre completo: {{nombreCompleto}}</p>
+        <p>nombre formato: {{nombreFormateado}}</p>
+        <p>nombre formato método: {{nombreFormateadoMetodo()}}</p>
+      </div>
+     </div> 
+
+     <div class="p-col-12 p-md-6 p-lg-4 p-fluid contact-form">
+      <Panel header="Contact Us">
+        <div class="p-grid">
+          <div class="p-col-12">
+            <Dropdown
+              v-model="dropdownCity"
+              :options="dropdownCities"
+              optionLabel="name"
+              placeholder="Select a City"
+            />
+          </div>
+          <div class="p-col-12">
+            <InputText type="text" placeholder="Name" />
+          </div>
+          <div class="p-col-12">
+            <InputText type="text" placeholder="Age" />
+          </div>
+          <div class="p-col-12">
+            <InputText type="text" placeholder="Message" />
+          </div>
+          <div class="p-col-12">
+            <Button type="button" label="Send" icon="pi pi-envelope" />
+          </div>
+        </div>
+      </Panel>
+    </div>
+    
+    <div class="p-col-6 p-lg-6">
+      <Panel header="Activity" style="height: 100%">
+        <div class="activity-header">
+          <div class="p-grid">
+            <div class="p-col-6">
+              <span style="font-weight: bold">Last Activity</span>
+              <p>Updated 1 minute ago</p>
+            </div>
+            <div class="p-col-6" style="text-align: right">
+              <Button label="Refresh" icon="pi pi-refresh" />
+            </div>
+          </div>
+        </div>
+
+        <ul class="activity-list">
+          <li>
+            <div class="p-d-flex p-jc-between p-ai-center p-mb-3">
+              <h5 class="activity p-m-0">Income</h5>
+              <div class="count">$900</div>
+            </div>
+            <ProgressBar :value="95" :showValue="false" />
+          </li>
+          <li>
+            <div class="p-d-flex p-jc-between p-ai-center p-mb-3">
+              <h5 class="activity p-m-0">Tax</h5>
+              <div class="count" style="background-color: #f9c851">$250</div>
+            </div>
+            <ProgressBar :value="24" :showValue="false" />
+          </li>
+        </ul>
+      </Panel>
+    </div>
 
     <button v-on:click="contador += 1">clic</button>
     <p>has hecho {{contador}} clics</p>
@@ -29,8 +93,34 @@
 </template>
 
 <script>
+import 'primevue/resources/themes/saga-blue/theme.css';
+import 'primevue/resources/primevue.css';
+import 'primeflex/primeflex.css';
+import 'primeicons/primeicons.css';
+
+import Checkbox from 'primevue/checkbox';
+import Panel from 'primevue/panel';
+import Dropdown from 'primevue/dropdown';
+import InputText from 'primevue/inputtext';
+import Button from 'primevue/button';
+import Column from 'primevue/column';
+import DataTable from 'primevue/datatable';
+import Chart from 'primevue/chart';
+import ProgressBar from 'primevue/progressbar';
+
 import axios from 'axios';
   export default {
+    components: {
+      ProgressBar,
+      Checkbox,
+      Panel,
+      Dropdown,
+      InputText,
+      Button,
+      Column,
+      DataTable,
+      Chart,
+    },
     setup(){ 
         },
     data() {
@@ -46,6 +136,14 @@ import axios from 'axios';
                 <br>
                 en Vue. Aquí un link:
                 <a href="//parzibyte.me/blog">parzibyte.me/blog</a>`,
+        dropdownCities: [
+          { name: 'New York', code: 'NY' },
+          { name: 'Rome', code: 'RM' },
+          { name: 'London', code: 'LDN' },
+          { name: 'Istanbul', code: 'IST' },
+          { name: 'Paris', code: 'PRS' },
+        ],
+        dropdownCity: null,
     }
     },
     methods: {
@@ -107,7 +205,7 @@ import axios from 'axios';
   }
 </script>
 
-<style>
+<style lang="scss" scoped>
   .myDiv{
     width: 180px;
     height: 60px;
@@ -122,4 +220,411 @@ import axios from 'axios';
   .noactive{
     border: solid 3px #555555
   }
+
+  
+@media screen and (max-width: 960px) {
+  ::v-deep(.p-datatable) {
+    &.p-datatable-customers {
+      .p-datatable-thead > tr > th,
+      .p-datatable-tfoot > tr > td {
+        display: none !important;
+      }
+
+      .p-datatable-tbody > tr {
+        border-bottom: 1px solid #dee2e6;
+        > td {
+          text-align: left;
+          display: flex;
+          align-items: center;
+          justify-content: center;
+          border: 0 none !important;
+          width: 100% !important;
+          float: left;
+          clear: left;
+          border: 0 none;
+
+          .p-column-title {
+            padding: 0.4rem;
+            min-width: 30%;
+            display: inline-block;
+            margin: -0.4rem 1rem -0.4rem -0.4rem;
+            font-weight: bold;
+          }
+
+          .p-progressbar {
+            margin-top: 0.5rem;
+          }
+        }
+      }
+    }
+  }
+}
+$fontSize: 14px;
+$bodyBgColor: #edf0f5;
+$textColor: #333333;
+$textSecondaryColor: #707070;
+$borderRadius: 3px;
+$dividerColor: #e3e3e3;
+$transitionDuration: 0.2s;
+$maskBgColor: #424242;
+$focusShadowColor: #8dcdff;
+
+/* Menu Common */
+$menuitemBadgeBgColor: #007be5;
+$menuitemBadgeColor: #ffffff;
+$submenuFontSize: 13px;
+$menuitemActiveRouteColor: #1fa1fc;
+
+/* Menu Light */
+$menuBgColorFirst: #f3f4f9;
+$menuBgColorLast: #d7dbe8;
+$menuitemColor: #232428;
+$menuitemHoverColor: #0388e5;
+$menuitemActiveColor: #0388e5;
+$menuitemActiveBgColor: #ffffff;
+$menuitemBorderColor: rgba(207, 211, 224, 0.6);
+
+/* Menu Dark */
+$menuDarkBgColorFirst: #4d505b;
+$menuDarkBgColorLast: #3b3e47;
+$menuitemDarkColor: #ffffff;
+$menuitemDarkHoverColor: #0388e5;
+$menuitemDarkActiveColor: #0388e5;
+$menuitemDarkActiveBgColor: #2e3035;
+$menuitemDarkBorderColor: rgba(52, 56, 65, 0.6);
+
+/* Topbar */
+$topbarLeftBgColor: #0388e5;
+$topbarRightBgColor: #07bdf4;
+$topbarItemBadgeBgColor: #ef6262;
+$topbarItemBadgeColor: #ffffff;
+$topbarItemColor: #ffffff;
+$topbarItemHoverColor: #c3e8fb;
+$topbarSearchInputBorderBottomColor: #ffffff;
+$topbarSearchInputColor: #ffffff;
+
+.card {
+  background-color: #ffffff;
+  padding: 1em;
+  margin-bottom: 16px;
+  border-radius: $borderRadius;
+
+  &.card-w-title {
+    padding-bottom: 2em;
+  }
+}
+
+h1,
+h2,
+h3,
+h4,
+h5,
+h6 {
+  margin: 1.5rem 0 1rem 0;
+  font-family: inherit;
+  font-weight: normal;
+  line-height: 1.2;
+  color: inherit;
+
+  &:first-child {
+    margin-top: 0;
+  }
+}
+
+h1 {
+  font-size: 2.5rem;
+}
+
+h2 {
+  font-size: 2rem;
+}
+
+h3 {
+  font-size: 1.75rem;
+}
+
+h4 {
+  font-size: 1.5rem;
+}
+
+h5 {
+  font-size: 1.25rem;
+}
+
+h6 {
+  font-size: 1rem;
+}
+@mixin icon-override($icon) {
+  &:before {
+    content: $icon;
+  }
+}
+
+@mixin linear-gradient($top, $bottom) {
+  background: $top; /* Old browsers */
+  background: linear-gradient(to bottom, $top 0%, $bottom 100%); /* W3C */
+  filter: progid:DXImageTransform.Microsoft.gradient( startColorstr='#ffffff', endColorstr='#000000',GradientType=0 ); /* IE6-9 */
+}
+
+@mixin linear-gradient-left($left, $right) {
+  background: $left; /* Old browsers */
+  background: linear-gradient(to right, $left 0%, $right 100%); /* W3C */
+  filter: progid:DXImageTransform.Microsoft.gradient( startColorstr=$left, endColorstr=$right,GradientType=1 ); /* IE6-9 */
+}
+
+@mixin opacity($opacity) {
+  opacity: $opacity;
+  $opacity-ie: $opacity * 100;
+  filter: alpha(opacity=$opacity-ie);
+}
+
+@mixin focused() {
+  outline: 0 none;
+  outline-offset: 0;
+  box-shadow: 0 0 0 0.2em $focusShadowColor;
+}
+
+@mixin focused-inset() {
+  outline: 0 none;
+  outline-offset: 0;
+  box-shadow: inset 0 0 0 0.2em $focusShadowColor;
+}
+
+@mixin clearfix {
+  &:after {
+    content: '';
+    display: table;
+    clear: both;
+  }
+}
+mark {
+  background: #fff8e1;
+  padding: 0.25rem 0.4rem;
+  border-radius: $borderRadius;
+  font-family: monospace;
+}
+
+blockquote {
+  margin: 1rem 0;
+  padding: 0 2rem;
+  border-left: 4px solid #90a4ae;
+}
+
+hr {
+  border-top: solid $dividerColor;
+  border-width: 1px 0 0 0;
+  margin: 1rem 0;
+}
+
+p {
+  margin: 0 0 1rem 0;
+  line-height: 1.5;
+
+  &:last-child {
+    margin-bottom: 0;
+  }
+}
+
+/* Code Highlight */
+.docs {
+  pre[class*='language-'] {
+    padding: 0 !important;
+    background: transparent;
+    overflow: visible;
+
+    > code {
+      border-left: 0 none;
+      box-shadow: none !important;
+      font-size: 14px;
+    }
+  }
+}
+/* Footer */
+$footerBgColor: #ffffff;
+.dashboard {
+  font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto, Helvetica,
+    Arial, sans-serif, Apple Color Emoji, Segoe UI Emoji, Segoe UI Symbol;
+  background-color: #edf0f5;
+  .summary {
+    position: relative;
+
+    .title {
+      font-size: 20px;
+    }
+
+    .detail {
+      color: $textSecondaryColor;
+      display: block;
+      margin-top: 10px;
+    }
+
+    .count {
+      color: #ffffff;
+      position: absolute;
+      top: 10px;
+      right: 10px;
+      font-size: 24px;
+      padding: 7px 14px;
+      border-radius: $borderRadius;
+
+      &.visitors {
+        background-color: #20d077;
+      }
+
+      &.purchases {
+        background-color: #f9c851;
+      }
+
+      &.revenue {
+        background-color: #007be5;
+      }
+    }
+  }
+
+  .highlight-box {
+    height: 100px;
+    display: flex;
+    @include clearfix();
+
+    .initials {
+      height: 100%;
+      width: 50%;
+      text-align: center;
+      padding: 1em;
+
+      > span {
+        font-size: 48px;
+      }
+    }
+
+    .highlight-details {
+      height: 100%;
+      background-color: #ffffff;
+      width: 50%;
+      padding: 1em;
+
+      i {
+        font-size: 18px;
+        vertical-align: middle;
+        margin-right: 0.5em;
+      }
+
+      .count {
+        color: $textSecondaryColor;
+        font-size: 36px;
+        margin-top: 4px;
+        display: block;
+      }
+    }
+  }
+
+  .task-list {
+    list-style-type: none;
+    margin: 0;
+    padding: 0;
+
+    li {
+      padding: 0.5em 0.25em;
+      border-bottom: 1px solid $dividerColor;
+      @include clearfix();
+    }
+
+    .p-checkbox {
+      vertical-align: middle;
+      margin-right: 0.5em;
+    }
+
+    .task-name {
+      vertical-align: middle;
+    }
+
+    i {
+      float: right;
+      font-size: 24px;
+      color: $textSecondaryColor;
+    }
+
+    .p-panel-content {
+      min-height: 256px;
+    }
+  }
+
+  .contact-form {
+    .p-panel-content {
+      min-height: 256px;
+    }
+  }
+
+  .contacts {
+    ul {
+      list-style-type: none;
+      padding: 0;
+      margin: 0;
+
+      li {
+        border-bottom: 1px solid $dividerColor;
+
+        button {
+          padding: 9px;
+          width: 100%;
+          box-sizing: border-box;
+          text-decoration: none;
+          position: relative;
+          display: block;
+          border-radius: 2px;
+          transition: background-color 0.2s;
+
+          .name {
+            position: absolute;
+            right: 10px;
+            top: 10px;
+            font-size: 18px;
+          }
+
+          .email {
+            position: absolute;
+            right: 10px;
+            top: 30px;
+            font-size: 14px;
+            color: $textSecondaryColor;
+          }
+
+          &:hover {
+            cursor: pointer;
+            background-color: #eeeeee;
+          }
+        }
+
+        &:last-child {
+          border: 0;
+        }
+      }
+    }
+
+    .p-panel-content {
+      min-height: 256px;
+    }
+  }
+
+  .activity-list {
+    list-style-type: none;
+    padding: 0;
+    margin: 0;
+
+    li {
+      border-bottom: 1px solid $dividerColor;
+      padding: 16px 8px;
+
+      .count {
+        font-size: 24px;
+        color: #ffffff;
+        background-color: #007be5;
+        font-weight: 700;
+        padding: 0.25em 0.5em;
+        display: inline-block;
+        border-radius: $borderRadius;
+      }
+    }
+  }
+}
 </style>
