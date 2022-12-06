11c82fb93658cc34e3e9fa235adb2db4fa9a0171
danilousach <danilo.verdugo@usach.cl>
2022-12-06 16:06:07 -0300

    modified:   paquetes/container/config/webpack.dev.js 	modified:   paquetes/container/public/index.html

--- paquetes/container/config/webpack.dev.js
+++ paquetes/container/config/webpack.dev.js
@@ -1,4 +1,5 @@
 const { merge } = require('webpack-merge');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 const ModuleFederationPlugin = require('webpack/lib/container/ModuleFederationPlugin');
 const commonConfig = require('./webpack.common');
 
@@ -18,6 +19,10 @@ const devConfig = {
         cart: 'cart@http://localhost:8082/remoteEntry.js',
         dashboard: 'dashboard@http://localhost:8083/remoteEntry.js',
       },
+      shared: ['react', 'react-dom'],
+    }),
+    new HtmlWebpackPlugin({
+      template: './public/index.html',
     }),
   ],
 };
